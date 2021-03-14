fdaee179dbdc8bd2b80e8ff4fbd16ba37cec3ed3
Abdulgafar Obeitor <aobeitor@yahoo.com>
2021-03-14 22:51:04 +0100

    quiz soln

--- src/Quiz.js
+++ src/Quiz.js
@@ -55,6 +55,7 @@ export default function Quiz({ code = "", goHome, onCodeChange }) {
     }
     else if(quizState === 'FINISHED' && submitStatus === 'READY'){
       console.log('call api to submit here')
+      console.log(quizSolutions)
       setTimeout(()=>{setSubmitStatus('DONE')},3000)
     }
   }, [quizState, code, password, submitStatus]);
